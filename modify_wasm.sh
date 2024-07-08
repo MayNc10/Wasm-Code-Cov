fe38aa9531c2188e5ea46ea952bf8be349c01bb1
May <MayflowerC10@gmail.com>
2024-07-08 15:51:32 -0400

    Shell script to automatically run wasm through counter adder (is hardcoded)

---
+++ modify_wasm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+cd $HOME/warm-code-cov/example-component
+cargo b 
+cd $HOME/warm-code-cov/wat-annotator
+cargo b --release
+NEW=$(wasm-tools print $HOME/warm-code-cov/example-component/target/wasm32-wasi/debug/example-component.wasm | $HOME/warm-code-cov/wat-annotator/target/release/wat-annotator)
+echo "${NEW}"
+cd ..
+echo "${NEW}" > modified.wasm

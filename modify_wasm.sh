d5f09497293f0521d76000025bd071ecbde20261
May Neelon <mayflowerc10@gmail.com>
2024-07-16 10:16:54 -0400

    Use a workspace to organize the packages

--- modify_wasm.sh
+++ modify_wasm.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e
-cd $HOME/warm-code-cov/example-component
-cargo component b
-cd $HOME/warm-code-cov/wat-annotator
-cargo b --release
-NEW=$(wasm-tools print $HOME/warm-code-cov/example-component/target/wasm32-wasi*/debug/example-component.wasm | $HOME/warm-code-cov/wat-annotator/target/release/wat-annotator)
+cd $HOME/warm-code-cov/
+cargo component build -p example-component
+cargo b --release -p wat-annotator
+NEW=$(wasm-tools print $HOME/warm-code-cov/target/wasm32-wasi*/debug/example-component.wasm | $HOME/warm-code-cov/target/release/wat-annotator)
 echo "${NEW}"
-cd ..
 echo "${NEW}" > modified.wat
 wasm-tools component wit modified.wat -o modified-world.wit
 wasm-tools component embed modified-world.wit --world root:component/root modified.wat -o modified.wasm

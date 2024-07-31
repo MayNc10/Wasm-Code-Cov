e097730f680035e53205750df0008f0e9509c6f7
May Neelon <mayflowerc10@gmail.com>
2024-07-31 16:00:56 -0400

    Rework offset tracker

--- wat-annotator/src/offset_tracker.rs
+++ wat-annotator/src/offset_tracker.rs
@@ -1,5 +1,4 @@
 use core::str;
-
 use regex::Regex;
 use wast::token::Index;
 
@@ -24,42 +23,22 @@ impl OffsetTracker {
         }
     }
 
-    // TODO rename this
-    fn find_break_idx(&self, original_loc: Location) -> Option<usize> {
-        let mut break_idx = None;
-        for idx in 0..self.offsets.len() {
-            let (off_loc, _) = self.offsets[idx];
-            if off_loc > original_loc {
-                break_idx = Some(idx);
-                break;
-            }
-        }
-
-        break_idx
+    fn get_real_loc(&self, start: Location) -> Location {
+        start
+            + self
+                .offsets
+                .iter()
+                .filter(|(loc, _)| *loc <= start)
+                .map(|(_, offset)| *offset)
+                .sum::<usize>()
     }
 
     // Should somehow link this struct to specific string
     // TODO!
     pub fn add_to_string(&mut self, s: &mut String, original_loc: Location, msg: &str) {
-        // figure out how much offset to add
-        let idx = self.find_break_idx(original_loc);
-        let end = match idx {
-            Some(idx) => {
-                self.offsets
-                    .insert(idx, (original_loc, msg.as_bytes().len()));
-                idx
-            }
-            None => {
-                self.offsets.push((original_loc, msg.as_bytes().len()));
-                self.offsets.len() - 1
-            }
-        };
-        let loc = original_loc
-            + self.offsets[..end]
-                .iter()
-                .map(|(_, offset)| offset)
-                .fold(0, |acc, offset| acc + offset);
+        let loc = self.get_real_loc(original_loc);
         s.insert_str(loc, msg);
+        self.offsets.push((original_loc, msg.as_bytes().len()));
     }
 
     // I feel kinda gross putting very implementation specific code here, but like
@@ -68,15 +47,7 @@ impl OffsetTracker {
         match idx {
             Index::Num(num, _) => {
                 if num >= lower_bound.unwrap_or(0) {
-                    let end = match self.find_break_idx(idx.span().offset()) {
-                        Some(i) => i,
-                        None => self.offsets.len(),
-                    };
-                    let loc = idx.span().offset()
-                        + self.offsets[..end]
-                            .iter()
-                            .map(|(_, offset)| offset)
-                            .fold(0, |acc, offset| acc + offset);
+                    let loc = self.get_real_loc(idx.span().offset());
 
                     let old = num.to_string();
                     for _ in 0..old.as_bytes().len() {
@@ -88,11 +59,7 @@ impl OffsetTracker {
                         idx.span().offset(),
                         new.as_bytes().len() - old.as_bytes().len(),
                     );
-                    if end == self.offsets.len() {
-                        self.offsets.push(tup);
-                    } else {
-                        self.offsets.insert(end, tup);
-                    }
+                    self.offsets.push(tup);
                 }
             }
             Index::Id(_) => {}
@@ -108,26 +75,13 @@ impl OffsetTracker {
     ) where
         F: FnOnce(&mut String, Location, Location) -> (Location, Offset),
     {
-        let idx = self.find_break_idx(loc);
-        let end = match idx {
-            Some(idx) => idx,
-            None => self.offsets.len(),
-        };
-        let loc = loc
-            + self.offsets[..end]
-                .iter()
-                .map(|(_, offset)| offset)
-                .fold(0, |acc, offset| acc + offset);
+        let loc = self.get_real_loc(loc);
         let slice = str::from_utf8_mut(unsafe { output[loc..].as_bytes_mut() }).unwrap();
         let m = re.find(slice);
         if let Some(m) = m {
             let (start, end) = (m.start(), m.end());
             let pair = f(output, loc + start, loc + end);
-            if idx.is_some() {
-                self.offsets.insert(idx.unwrap(), pair);
-            } else {
-                self.offsets.push(pair);
-            }
+            self.offsets.push(pair);
         }
     }
 
